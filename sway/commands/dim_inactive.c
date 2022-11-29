5667728e3d2d7862c98752ce1ec845105c41a8de
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2022-11-30 00:03:20 +0100

    Mark all outputs as damaged to "hot-reload" when changing dim_inactive

--- sway/commands/dim_inactive.c
+++ sway/commands/dim_inactive.c
@@ -2,6 +2,12 @@
 #include "sway/commands.h"
 #include "sway/config.h"
 #include "log.h"
+#include "sway/output.h"
+
+static void rebuild_textures_iterator(struct sway_container *con, void *data) {
+	container_update_marks_textures(con);
+	container_update_title_textures(con);
+}
 
 struct cmd_results *cmd_dim_inactive(int argc, char **argv) {
 	struct cmd_results *error = NULL;
@@ -16,6 +22,15 @@ struct cmd_results *cmd_dim_inactive(int argc, char **argv) {
 	}
 
 	config->dim_inactive = val;
+
+	if (config->active) {
+		root_for_each_container(rebuild_textures_iterator, NULL);
+
+		for (int i = 0; i < root->outputs->length; ++i) {
+			struct sway_output *output = root->outputs->items[i];
+			output_damage_whole(output);
+		}
+	}
 
 	return cmd_results_new(CMD_SUCCESS, NULL);
 }
