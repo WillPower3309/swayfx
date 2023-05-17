acafb20b114ea93f971e118da0233c07157286c0
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2023-05-18 01:39:48 +0200

    Move stencil into each framebuffer (#156)
    * Move stencil into each framebuffer
    
    Also fixes the stencil being added to the wrong framebuffer
    
    * Initialize texture members on framebuffer init
    
    * removed bind arg
    
    * renamed init to create, changed existing create to update
    
    * moved stencil buffer creation to new function
    
    * removed some now misleading comments
    
    ---------
    
    Co-authored-by: William McKinnon <contact@willmckinnon.com>

--- include/sway/desktop/fx_renderer/fx_framebuffer.h
+++ include/sway/desktop/fx_renderer/fx_framebuffer.h
@@ -10,11 +10,16 @@
 struct fx_framebuffer {
 	struct fx_texture texture;
 	GLuint fb;
+	GLuint stencil_buffer;
 };
+
+struct fx_framebuffer fx_framebuffer_create();
 
 void fx_framebuffer_bind(struct fx_framebuffer *buffer);
 
-void fx_framebuffer_create(struct fx_framebuffer *buffer, int width, int height, bool bind);
+void fx_framebuffer_update(struct fx_framebuffer *buffer, int width, int height);
+
+void fx_framebuffer_add_stencil_buffer(struct fx_framebuffer *buffer, int width, int height);
 
 void fx_framebuffer_release(struct fx_framebuffer *buffer);
 
