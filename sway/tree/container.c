59010af70f71dedcc1b279a9320617421e2a8d48
William McKinnon <contact@willmckinnon.com>
2025-01-10 18:29:16 -0500

    fixed tabbed titlebar bug

--- sway/tree/container.c
+++ sway/tree/container.c
@@ -357,7 +357,7 @@ void container_arrange_title_bar(struct sway_container *con) {
 	enum corner_location corners = CORNER_LOCATION_TOP;
 	if (con->current.parent) {
 		list_t *siblings = con->current.parent->current.children;
-		if (con->current.parent->current.layout == L_TABBED) {
+		if (con->current.parent->current.layout == L_TABBED && siblings->length > 1) {
 			if (siblings->items[0] == con) {
 				corners = CORNER_LOCATION_TOP_LEFT;
 			} else if (siblings->items[siblings->length - 1] == con) {
