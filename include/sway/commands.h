5bf9d204e03a1fa882ee63da3978da2d1f01870e
William McKinnon <contact@willmckinnon.com>
2025-02-25 17:56:18 -0500

    added animation_duration_ms config

--- include/sway/commands.h
+++ include/sway/commands.h
@@ -109,6 +109,7 @@ sway_cmd cmd_exec_validate;
 sway_cmd cmd_exec_process;
 
 sway_cmd cmd_allow_tearing;
+sway_cmd cmd_animation_duration_ms;
 sway_cmd cmd_assign;
 sway_cmd cmd_bar;
 sway_cmd cmd_bindcode;
