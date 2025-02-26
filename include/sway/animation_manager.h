c26fb5fe7c136f1805ffc25bc34f2b9f3e694507
William McKinnon <contact@willmckinnon.com>
2025-02-26 00:41:08 -0500

    refactor container_animation_state with callbacks

--- include/sway/animation_manager.h
+++ include/sway/animation_manager.h
@@ -4,24 +4,41 @@
 #include "list.h"
 #include "sway/tree/container.h"
 
-// TODO: have target var so we can have the same struct work w alpha and pos
-// then rename from_alpha and to_alpha to from and to
+/* TODO: add an array of the below to the animation state
+struct animated_var {
+	float from;
+	float to;
+	float *current;
+};
+ */
+
 struct container_animation_state {
-	float from_alpha;
-	float to_alpha;
-	double progress;
-	bool is_being_animated;
+	struct wl_list link;
+	bool init;
+	float from;
+	float to;
+	float *current;
+	float progress;
 	struct sway_container *container;
+	void (*update)(struct sway_container *con);
+	void (*complete)(struct sway_container *con);
 };
 
 struct animation_manager {
 	struct wl_event_source *tick;
-	list_t *animated_states; // struct container_animation_state
+	struct wl_list animation_states; // struct container_animation_state
 };
 
 struct animation_manager *animation_manager_create();
 
+struct container_animation_state container_animation_state_create_fadein(struct sway_container *con);
+
+struct container_animation_state container_animation_state_create_fadeout(struct sway_container *con);
+
 void add_container_animation(struct container_animation_state *animation_state,
+		struct animation_manager *animation_manager);
+
+void cancel_container_animation(struct container_animation_state *animation_state,
 		struct animation_manager *animation_manager);
 
 #endif
