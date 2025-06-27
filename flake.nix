04da086f5f8ceb3cefe6d9225ff6f49d16b0913c
William McKinnon <contact@willmckinnon.com>
2025-06-26 23:37:09 -0400

    fixed + simplified flake

--- flake.nix
+++ flake.nix
@@ -1,110 +1,86 @@
 {
   description = "Swayfx development environment";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    systems.url = "github:nix-systems/default-linux";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
     scenefx = {
       url = "github:wlrfx/scenefx";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      scenefx,
-      ...
-    }:
-    let
-      mkPackage = pkgs: {
-        swayfx-unwrapped =
-          (pkgs.swayfx-unwrapped.override {
-            # This override block is currently empty, you might remove it
-            # unless you intend to override function arguments later.
-          }).overrideAttrs
-            (old: {
-              version = "0.5.2-git";
-              src = pkgs.lib.cleanSource ./.;
-              nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.cmake ];
-              # Add wlroots_0_19 here
-              buildInputs = old.buildInputs ++ [
-                pkgs.scenefx
-                pkgs.wlroots_0_19 # <-- Added this line
-              ];
-              providedSessions = [ pkgs.swayfx-unwrapped.meta.mainProgram ];
-              patches = []; ## Consider if you need patches from the original derivation
-              mesonFlags = let
-                inherit (pkgs.lib.strings) mesonEnable mesonOption;
-              in
-                [
-                  (mesonOption "sd-bus-provider" "libsystemd")
-                  (mesonEnable "tray" true)
-                  # You might need to explicitly enable fx if the default changed
-                  # (mesonEnable "fx" true) # <-- Potentially add this if needed
-                ];
-            });
-      };
 
-      targetSystems = [
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
-      pkgsFor =
-        system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ scenefx.overlays.insert ];
+  outputs = { self, nixpkgs, flake-utils, scenefx, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        scenefx-git = scenefx.packages.${system}.scenefx-git;
+
+      in {
+        packages = rec {
+          swayfx-unwrapped-git = pkgs.swayfx-unwrapped.overrideAttrs(old: {
+            version = "git";
+            src = pkgs.lib.cleanSource ./.;
+            nativeBuildInputs = with pkgs; [
+              meson
+              ninja
+              pkg-config
+              wayland-scanner
+              scdoc
+            ];
+            buildInputs = with pkgs; [
+              libGL
+              wayland
+              libxkbcommon
+              pcre2
+              json_c
+              libevdev
+              pango
+              cairo
+              libinput
+              gdk-pixbuf
+              librsvg
+              wayland-protocols
+              libdrm
+              xorg.xcbutilwm
+              wlroots_0_19
+            ] ++ [ scenefx-git ];
+          });
+
+          default = swayfx-unwrapped-git;
         };
-      forEachSystem = f: nixpkgs.lib.genAttrs targetSystems (system: f (pkgsFor system));
-    in
-    {
-      overlays = rec {
-        default = insert;
-        # Insert using the locked nixpkgs. Can be used with any nixpkgs version.
-        insert = _: prev: mkPackage (pkgsFor prev.system);
-        # Override onto the input nixpkgs. Users *MUST* have a scenefx overlay
-        # used before this overlay, otherwise pkgs.scenefx will be unavailable
-        override = _: prev: mkPackage prev;
-      };
 
-      packages = forEachSystem (
-        pkgs: (mkPackage pkgs // { default = self.packages.${pkgs.system}.swayfx-unwrapped; })
-      );
-
-      devShells = forEachSystem (pkgs: {
-        default = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           name = "swayfx-shell";
-          # inputsFrom propagates buildInputs, nativeBuildInputs etc. from the listed derivations
-          # Adding wlroots and scenefx explicitly here is fine, but they are also included via inputsFrom
           inputsFrom = [
-            self.packages.${pkgs.system}.swayfx-unwrapped
-            # pkgs.wlroots_0_19 # Included via swayfx-unwrapped buildInputs now
-            # pkgs.scenefx      # Included via swayfx-unwrapped buildInputs now
+            self.packages.${system}.swayfx-unwrapped-git
+            scenefx-git
           ];
-          # You still might want wlroots/scenefx here if you need tools/headers directly in the shell
-          # outside of what swayfx uses.
-          buildInputs = [
-            pkgs.wlroots_0_19
-            pkgs.scenefx
-          ];
-          packages = with pkgs; [
-            gdb # for debugging
-          ];
+          packages = [ pkgs.gdb ]; # add debugging packages
           shellHook = ''
             (
               # Copy the nix version of wlroots and scenefx into the project
               # This is useful if you want meson to use them as subprojects during manual dev/testing
-              echo "Copying wlroots and scenefx sources to ./subprojects for dev environment..."
               mkdir -p "$PWD/subprojects" && cd "$PWD/subprojects"
-              rm -rf wlroots scenefx # Clean previous copies if they exist
-              cp -R --no-preserve=mode,ownership ${pkgs.wlroots_0_19.src} wlroots
-              cp -R --no-preserve=mode,ownership ${pkgs.scenefx.src} scenefx
-              echo "Done copying sources."
+              if [ ! -d scenefx ]; then
+                echo "Copying scenefx to ./subprojects..."
+                cp -R --no-preserve=mode,ownership ${scenefx-git.src} scenefx
+              fi
               cd "$OLDPWD"
             ) || echo "Failed to copy subproject sources."
           '';
         };
-      });
 
-      formatter = forEachSystem (pkgs: pkgs.nixfmt-rfc-style);
-    };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
+
