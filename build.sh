2a62551325218a5515e20df839c46357ff3f2ebb
Klaus Umbach <klaus-github@uxix.de>
2026-01-17 22:14:58 +0100

    fix: build in container on other architectures
    Before this change, building only worked on x86_64.
    Should now be generic. Tested on x86_64 and aarch64.
    
    Also adds a variable (OPT_SWAYFX), to install in other directories than
    /opt. Yes, I know this is not the cleanest way, to have a "fix" and an
    "add" in the same commit. If you really, really insist, I can make two
    PRs out of it, but it would be more work, than the changes itself.

--- build.sh
+++ build.sh
@@ -5,34 +5,36 @@
 set -eE -u -o pipefail
 shopt -s inherit_errexit
 
-meson setup --prefix /opt "wlroots-${WLROOTSVERSION}/build/" "wlroots-${WLROOTSVERSION}"
+DEB_BUILD_GNU_TYPE=$(dpkg-architecture -q DEB_BUILD_GNU_TYPE)
+
+meson setup --prefix ${OPT_SWAYFX} "wlroots-${WLROOTSVERSION}/build/" "wlroots-${WLROOTSVERSION}"
 ninja -C "wlroots-${WLROOTSVERSION}/build/"
 ninja -C "wlroots-${WLROOTSVERSION}/build/" install
-ls -l /opt/lib/x86_64-linux-gnu/pkgconfig
+ls -l ${OPT_SWAYFX}/lib/${DEB_BUILD_GNU_TYPE}/pkgconfig
 echo "wlroots DONE"
 
-PKG_CONFIG_PATH="/opt/lib/x86_64-linux-gnu/pkgconfig/${PKG_CONFIG_PATH+:${PKG_CONFIG_PATH}}" \
-meson setup --prefix /opt --cmake-prefix-path /opt "scenefx-${SCENEFXVERSION}/build/" "scenefx-${SCENEFXVERSION}"
+PKG_CONFIG_PATH="${OPT_SWAYFX}/lib/${DEB_BUILD_GNU_TYPE}/pkgconfig/${PKG_CONFIG_PATH+:${PKG_CONFIG_PATH}}" \
+meson setup --prefix ${OPT_SWAYFX} --cmake-prefix-path ${OPT_SWAYFX} "scenefx-${SCENEFXVERSION}/build/" "scenefx-${SCENEFXVERSION}"
 ninja -C "scenefx-${SCENEFXVERSION}/build/"
 ninja -C "scenefx-${SCENEFXVERSION}/build/" install
 echo "scenefx DONE"
 
-PKG_CONFIG_PATH="/opt/lib/x86_64-linux-gnu/pkgconfig/${PKG_CONFIG_PATH+:${PKG_CONFIG_PATH}}" \
-meson setup --prefix /opt --cmake-prefix-path /opt "swayfx-${SWAYFXVERSION}/build/" "swayfx-${SWAYFXVERSION}"
+PKG_CONFIG_PATH="${OPT_SWAYFX}/lib/${DEB_BUILD_GNU_TYPE}/pkgconfig/${PKG_CONFIG_PATH+:${PKG_CONFIG_PATH}}" \
+meson setup --prefix ${OPT_SWAYFX} --cmake-prefix-path ${OPT_SWAYFX} "swayfx-${SWAYFXVERSION}/build/" "swayfx-${SWAYFXVERSION}"
 ninja -C "swayfx-${SWAYFXVERSION}/build/"
 ninja -C "swayfx-${SWAYFXVERSION}/build/" install
 echo "swayfx DONE"
 
-mv /opt/bin/sway /opt/bin/swayfx
+mv ${OPT_SWAYFX}/bin/sway ${OPT_SWAYFX}/bin/swayfx
 
-cat >/opt/bin/swayfx.sh <<"EOF"
+cat >${OPT_SWAYFX}/bin/swayfx.sh <<EOF
 #!/bin/sh
-export PATH=/opt/bin:${PATH}
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/lib/x86_64-linux-gnu/
-exec /opt/bin/swayfx
+export PATH=${OPT_SWAYFX}/bin:\${PATH}
+export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:${OPT_SWAYFX}/lib/${DEB_BUILD_GNU_TYPE}/
+exec ${OPT_SWAYFX}/bin/swayfx
 EOF
 
-chmod 755 /opt/bin/swayfx.sh
+chmod 755 ${OPT_SWAYFX}/bin/swayfx.sh
 
 echo
-echo "Successfully build swayfx. Find it in your hosts bind directory (default: ./opt)."
+echo "Successfully build swayfx. Find it in your hosts bind directory (default: .${OPT_SWAYFX})."
